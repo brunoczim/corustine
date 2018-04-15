6cdde634c53fe4f794e4b719f4230156bbc4a190
Bruno Corrêa Zimmermann <brunoczim@gmail.com>
2018-04-15 13:44:15 -0300

    corrected doc location

--- docs/implementors/core/marker/trait.Copy.js
+++ docs/implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["coro"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"coro/struct.CoId.html\" title=\"struct coro::CoId\">CoId</a>",synthetic:false,types:["coro::CoId"]},];
+implementors["corustine"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"corustine/struct.CoId.html\" title=\"struct corustine::CoId\">CoId</a>",synthetic:false,types:["corustine::CoId"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
